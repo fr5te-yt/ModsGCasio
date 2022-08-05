e3acbd788e46465ec9076804788f57a9eba9d41a
fr5te-yt <77345783+fr5te-yt@users.noreply.github.com>
2022-08-05 20:06:45 +0200

    MISES A JOUR 0.0.1
    Co-Authored-By: Northeen <67755377+Northeeen@users.noreply.github.com>

--- src/main/java/net/mcreator/gcasio/init/GcasioModBlocks.java
+++ src/main/java/net/mcreator/gcasio/init/GcasioModBlocks.java
@@ -7,13 +7,27 @@ package net.mcreator.gcasio.init;
 import net.minecraftforge.registries.RegistryObject;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.api.distmarker.Dist;
 
 import net.minecraft.world.level.block.Block;
 
 import net.mcreator.gcasio.block.MineraisdeCasioBlock;
+import net.mcreator.gcasio.block.CaveBlockBlock;
 import net.mcreator.gcasio.GcasioMod;
 
 public class GcasioModBlocks {
 	public static final DeferredRegister<Block> REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCKS, GcasioMod.MODID);
 	public static final RegistryObject<Block> MINERAISDE_CASIO = REGISTRY.register("mineraisde_casio", () -> new MineraisdeCasioBlock());
+	public static final RegistryObject<Block> CAVE_BLOCK = REGISTRY.register("cave_block", () -> new CaveBlockBlock());
+
+	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+	public static class ClientSideHandler {
+		@SubscribeEvent
+		public static void clientSetup(FMLClientSetupEvent event) {
+			CaveBlockBlock.registerRenderLayer();
+		}
+	}
 }
